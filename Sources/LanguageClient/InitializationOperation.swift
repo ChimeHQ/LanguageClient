ec8ed2dbdde99de99be2cbd81088346ea432792f
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-09-27 08:43:34 -0400

    Ensure that InitializationOperation handles failures

--- Sources/LanguageClient/InitializationOperation.swift
+++ Sources/LanguageClient/InitializationOperation.swift
@@ -3,7 +3,7 @@ import OperationPlus
 import LanguageServerProtocol
 
 @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-class InitializationOperation: AsyncProducerOperation<Result<InitializationResponse, ServerError>> {
+final class InitializationOperation: AsyncProducerOperation<Result<InitializationResponse, Error>> {
     typealias InitializeParamsProvider = InitializingServer.InitializeParamsProvider
 
     let initializeParamsProvider: InitializeParamsProvider
@@ -16,47 +16,17 @@ class InitializationOperation: AsyncProducerOperation<Result<InitializationRespo
 
     override func main() {
 		Task {
-			let params = try await initializeParamsProvider()
+			do {
+				let params = try await initializeParamsProvider()
 
-			let initResponse = try await server.initialize(params: params)
+				let initResponse = try await server.initialize(params: params)
 
-			try await server.initialized(params: InitializedParams())
+				try await server.initialized(params: InitializedParams())
 
-			self.finish(with: .success(initResponse))
+				self.finish(with: .success(initResponse))
+			} catch {
+				self.finish(with: .failure(error))
+			}
 		}
-
-//        getInitializeParams({ (params) in
-//            self.server.initialize(params: params) { result in
-//                switch result {
-//                case .failure(let error):
-//                    self.finish(with: .failure(error))
-//                case .success(let response):
-//                    self.sendInitNotification(response: response)
-//                }
-//            }
-//        })
-
     }
-
-//    private func getInitializeParams(_ block: @escaping (InitializeParams) -> Void) {
-//        initializeParamsProvider({ (result) in
-//            switch result {
-//            case .failure(let error):
-//                self.finish(with: .failure(.clientDataUnavailable(error)))
-//            case .success(let params):
-//                block(params)
-//            }
-//        })
-//    }
-//
-//    private func sendInitNotification(response: InitializationResponse) {
-//        server.initialized(params: InitializedParams()) { error in
-//            if let error = error {
-//                self.finish(with: .failure(error))
-//                return
-//            }
-//
-//            self.finish(with: .success(response))
-//        }
-//    }
 }
