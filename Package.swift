cc94fe971d36eea05df6f2da8a60eadfe291a18b
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-09-30 06:47:19 -0400

    Bump platform versions to keep compiler happy

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "LanguageClient",
-    platforms: [.macOS(.v10_13), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
+    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11), .watchOS(.v4)],
     products: [
         .library(
             name: "LanguageClient",
